ae584dbf49a819804d157cdafa652dac9fcfbec1
DiLDoST <wahagalbaluch33@gmail.com>
2020-10-22 04:23:19 +0500

    init

---
+++ DSIJokes/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+/*
+ *
+ * Project Created By DiLDoST Wahag
+ * On 22 Oct 2020 at 4:20AM
+ *
+ * Using AIDE
+ * 
+ */
+
+public class Main
+{
+	static Scanner input = new Scanner(System.in);
+
+	public static void main(String[] args)
+	{
+		System.out.println("Hi and Welcome to DSI-Jokes Console App!");
+	}
+}
